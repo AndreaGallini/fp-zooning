3a3f57ba44838e6505f41204bca82c801a8a9675
AndreaGallini <75275610+AndreaGallini@users.noreply.github.com>
2024-08-26 15:40:06 +0200

    Update settings.py

--- fp_zooning/settings.py
+++ fp_zooning/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)#7k+1c8(efk0a)z@_5^zhfb$phsqg3(hzsk^77f$8rl3!jp0^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 LANGUAGE_CODE = 'it'
 # Application definition
 
