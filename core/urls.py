08fee6b7f307b240e20a0e404ad431991225f530
AndreaGallini <75275610+AndreaGallini@users.noreply.github.com>
2024-08-27 14:33:50 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('delete/', views.delete_all_data, name='delete_all_data'),
+    path('export/', views.export_data, name='export_data'),
 ]
